1494a00818229e2661c7d272d0504a98c20aba26
Addi Trejo <125314660+Additrejo@users.noreply.github.com>
2025-04-08 13:47:18 -0600

    Add files via upload

--- Matlab Codes/muestras_voltaje.m
+++ Matlab Codes/muestras_voltaje.m
@@ -1,6 +1,7 @@
-% Código para obtener y graficar 500 muestras del voltaje de una batería 18650 en thingspeak,
+% Código para obtener y graficar 500 muestras del voltaje de una batería 18650 en thingspeak ,
 % consulte el repositorio para su funcionamiento.
-%Autor: Addi Trejo, Mar,2025
+% Autor: Addi Trejo, 08 Abril,2025
+% Ult Actu: 08 Abril 2025
 
 
 channelID = 2805725; % Reemplaza con tu Channel ID
@@ -11,7 +12,7 @@ data = thingSpeakRead(channelID, 'Fields', fieldID, 'NumPoints', 500, 'ReadKey',
 % Crear un gráfico
 figure;
 plot(data, '-o');
-title('Visualización de Datos desde ThingSpeak');
+title('Voltaje en 500 muestras');
 xlabel('Número de Muestra');
 ylabel('Volts [V]');
 grid on;
